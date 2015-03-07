2295a0b27de4759a518eb83fcaddab571b8ee063
Nav Bhatti <bhatti.nav@gmail.com>
2015-03-06 20:17:05 -0800

    cache commit
    Signed-off-by: Nav Bhatti <bhatti.nav@gmail.com>

---
+++ ass2/Controllers/RoleController.cs
@@ -0,0 +1,20 @@
+﻿using ass2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ass2.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            return View(db.Roles.ToList());
+        }
+    }
+}
---
+++ ass2/Migrations/SimConfig/201503062122444_InitialCreate.cs
@@ -0,0 +1,356 @@
+namespace ass2.Migrations.SimConfig
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.AbuserRelationships",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Ages",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.AssignedWorkers",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.BadDateReports",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.CityOfAssaults",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.CityOfResidences",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Crises",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.DrugFacilitatedAssaults",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.DuplicateFiles",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Ethnicities",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.EvidenceStoreds",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.FamilyViolenceFiles",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.HIVMeds",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.HospitalAttendeds",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Incidents",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.MedicalOnlies",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.MultiplePerpetrators",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.PoliceAttendances",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.PoliceReporteds",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Programs",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.ReferralContacts",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.ReferralSources",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.ReferredToCBVS",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.ReferringHospitals",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.RepeatClients",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.RiskLevels",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.RiskStatus",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Services",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.SexWorkExploitations",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.SocialWorkAttendances",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.StatusOfFiles",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.ThirdPartyReports",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.VictimOfIncidents",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.VictimServicesAttendances",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        value = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.VictimServicesAttendances");
+            DropTable("dbo.VictimOfIncidents");
+            DropTable("dbo.ThirdPartyReports");
+            DropTable("dbo.StatusOfFiles");
+            DropTable("dbo.SocialWorkAttendances");
+            DropTable("dbo.SexWorkExploitations");
+            DropTable("dbo.Services");
+            DropTable("dbo.RiskStatus");
+            DropTable("dbo.RiskLevels");
+            DropTable("dbo.RepeatClients");
+            DropTable("dbo.ReferringHospitals");
+            DropTable("dbo.ReferredToCBVS");
+            DropTable("dbo.ReferralSources");
+            DropTable("dbo.ReferralContacts");
+            DropTable("dbo.Programs");
+            DropTable("dbo.PoliceReporteds");
+            DropTable("dbo.PoliceAttendances");
+            DropTable("dbo.MultiplePerpetrators");
+            DropTable("dbo.MedicalOnlies");
+            DropTable("dbo.Incidents");
+            DropTable("dbo.HospitalAttendeds");
+            DropTable("dbo.HIVMeds");
+            DropTable("dbo.FamilyViolenceFiles");
+            DropTable("dbo.EvidenceStoreds");
+            DropTable("dbo.Ethnicities");
+            DropTable("dbo.DuplicateFiles");
+            DropTable("dbo.DrugFacilitatedAssaults");
+            DropTable("dbo.Crises");
+            DropTable("dbo.CityOfResidences");
+            DropTable("dbo.CityOfAssaults");
+            DropTable("dbo.BadDateReports");
+            DropTable("dbo.AssignedWorkers");
+            DropTable("dbo.Ages");
+            DropTable("dbo.AbuserRelationships");
+        }
+    }
+}
